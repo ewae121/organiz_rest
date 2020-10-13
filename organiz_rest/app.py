f57acf90d68c7be1932e6054ad19ef74bbfda052
Bousquet <bousquet.laurent12@gmail.com>
2020-10-13 13:37:25 +0200

    [FEAT] Cors appent

--- organiz_rest/app.py
+++ organiz_rest/app.py
@@ -1,9 +1,11 @@
 import connexion
+from flask_cors import CORS
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir='../swaggers')
 
 app.add_api("swagger.yml")
+CORS(app.app)
 
 
 # If we're running in stand alone mode, run the application
